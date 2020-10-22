5072fbe05afc22b7aac9130ad70fd62879dbecb4
tkmmark <marktam@gmail.com>
2020-10-22 20:12:48 +0200

    Update cache.py

--- examples/cache.py
+++ examples/cache.py
@@ -11,7 +11,7 @@ pts = [[0,0,0], [1,0,0]]
 pts_cache = proxy.cache(pts) # cache the object to server side and return its reference
 print(pts_cache) # will print: {'cached': some_unique_id}
 
-T = Translation([100, 0, 0]).matrix
+T = Translation().from_vector([100, 0, 0]).matrix
 result = transform_points_numpy(pts_cache, T) # call the function through proxy
 print(result) # will print: [[100.0, 0.0 ,0.0], [101.0, 0.0, 0.0]]
 
@@ -26,7 +26,7 @@ pts = [[0,0,0], [1,0,0]]
 pts_cache = proxy.cache(pts)
 print(pts_cache) # will print: {'cached': some_unique_id}
 
-T = Translation([100, 0, 0]).matrix
+T = Translation().from_vector([100, 0, 0]).matrix
 result_cache = transform_points_numpy(pts_cache, T) # call the function through proxy
 print(result_cache) # will print: {'cached': some_unique_id}
 
